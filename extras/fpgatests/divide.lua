8353d30634cd815190e8a144490c3fb9afd41691
James Hegarty <jameshegarty@gmail.com>
2014-11-10 22:57:50 -0800

    divide and lut tests

--- extras/fpgatests/divide.lua
+++ extras/fpgatests/divide.lua
@@ -1,8 +1,8 @@
 (terralib.loadfile("test.t"))()
 import "darkroom"
 
-im a(x,y) ((testinput/([uint8](100)))/testinput) end
-im b(x,y) ([uint16](a))/([uint16](testinput+[uint16](3))) end
-im c(x,y) ([uint32](testinput))/([uint32](b)) end
+im a(x,y) ((testinput+[uint8](100))/([uint8](2))) / (testinput(x-10,y)+([uint8](1))) end
+im b(x,y) ([uint16](a)*[uint16](100)) / ([uint16](testinput(x-10,y)+[uint16](3))) end
+im c(x,y) ([int32](testinput*100)) / ([int32](b(x-10,y))+1) end
 
-test(c)
+test(im(x,y) [uint8](b*100) end)
---
+++ extras/fpgatests/taplut.lua
@@ -0,0 +1,9 @@
+(terralib.loadfile("test.t"))()
+import "darkroom"
+
+local st = {}
+for i=0,255 do table.insert(st, i*i/(255)) end
+
+local square = darkroomSimple.tapLUT( uint8, st )
+
+test(im(x,y) square[testinput(x,y)] end)
