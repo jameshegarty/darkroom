7b699259b71f17a2e73121fd3369bada33925f10
James Hegarty <jameshegarty@gmail.com>
2014-12-02 01:45:35 -0800

    fixed mr_composed

--- extras/fpgatests/mr_composed.lua
+++ extras/fpgatests/mr_composed.lua
@@ -8,7 +8,7 @@ local targetY = 1
 test(im(x,y)
      [uint8](map i = 0,2 reduce(sum)
       map ii=-1,1 jj=-1,1 reduce(sum) -- SAD
-        testinput(x+i+ii,y+jj)
+      testinput(x+i+ii,y+jj)+[uint8](3)
       end
       end)
 end)
