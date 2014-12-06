87a979b794a377f93f6975dc0c4dd95356e4d586
James Hegarty <jameshegarty@gmail.com>
2014-12-05 23:03:51 -0800

    iterate tests

---
+++ extras/fpgatests/iterate_convolve.lua
@@ -0,0 +1,14 @@
+(terralib.loadfile("test.t"))()
+import "darkroom"
+
+-- sums to 256
+local K={1,4,7,4,1,
+                         4,15,24,15,4,
+                         7,24,36,24,7,
+                         4,15,24,15,4,
+                         1,4,7,4,1} 
+
+test(im(x,y) iterate i=0,5 A=darkroom.gatherColumn(testinput,i-2,1) reduce(sum)
+     map j=-2,2 reduce(sum) [uint8](([uint16](A[j+2])*[uint16](K[(j+2)*5 + i]))>>[uint16](8)) end
+     end
+end)
